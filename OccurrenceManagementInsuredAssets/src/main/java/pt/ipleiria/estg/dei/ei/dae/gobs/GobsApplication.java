627361510251ef3ea4e6f30571587af21e4000c3
CapitaoFTW <rodrigofilipecapitao@gmail.com>
2022-12-27 16:06:36 +0000

    deleted old folder

--- OccurrenceManagementInsuredAssets/src/main/java/pt/ipleiria/estg/dei/ei/dae/gobs/GobsApplication.java
+++
@@ -1,8 +0,0 @@
-package pt.ipleiria.estg.dei.ei.dae.gobs;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class GobsApplication extends Application {
-}
--- OccurrenceManagementInsuredAssets/src/main/java/pt/ipleiria/estg/dei/ei/dae/gobs/api/EstadoOcorrencia.java
+++
@@ -1,9 +0,0 @@
-package pt.ipleiria.estg.dei.ei.dae.gobs.api;
-
-public enum EstadoOcorrencia {
-    CRIADA,
-    ANALISADA,
-    REPARACAO,
-    REPARADA,
-    CONCLUIDA
-}
