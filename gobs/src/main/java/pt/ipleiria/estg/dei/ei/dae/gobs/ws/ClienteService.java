fa73bf18a3af5eeedf1590a92d4207e189f4ce29
Rúben Garcia <rube200@users.noreply.github.com>
2023-01-09 03:54:47 +0000

    Use ClienteDTO

--- gobs/src/main/java/pt/ipleiria/estg/dei/ei/dae/gobs/ws/ClienteService.java
+++ gobs/src/main/java/pt/ipleiria/estg/dei/ei/dae/gobs/ws/ClienteService.java
@@ -1,25 +1,35 @@
 package pt.ipleiria.estg.dei.ei.dae.gobs.ws;
 
+import pt.ipleiria.estg.dei.ei.dae.gobs.dtos.ClienteDTO;
 import pt.ipleiria.estg.dei.ei.dae.gobs.ejbs.ClienteBean;
 import pt.ipleiria.estg.dei.ei.dae.gobs.entities.Cliente;
 import pt.ipleiria.estg.dei.ei.dae.gobs.exceptions.GobsEntityNotFoundException;
+import pt.ipleiria.estg.dei.ei.dae.gobs.security.Authenticated;
 
+import javax.annotation.security.RolesAllowed;
 import javax.ejb.EJB;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
+import static pt.ipleiria.estg.dei.ei.dae.gobs.ejbs.AuthBean.CLIENTE_ROLE;
+
+@Authenticated
+@Consumes({MediaType.APPLICATION_JSON})
 @Path("clientes")
 @Produces({MediaType.APPLICATION_JSON})
-@Consumes({MediaType.APPLICATION_JSON})
-public class ClienteService {//todo DTOs add Authentication
+@RolesAllowed({CLIENTE_ROLE})
+public class ClienteService {
     @EJB
     private ClienteBean clienteBean;
 
     @GET
     @Path("/")
     public Response getAllClientes() {
-        return Response.ok(clienteBean.getClientes()).build();//todo dto
+        Collection<ClienteDTO> clienteDTOs = clienteBean.getClientes().stream().map(Cliente::toDto).collect(Collectors.toList());
+        return Response.ok(clienteDTOs).build();
     }
 
     @GET
@@ -29,6 +39,6 @@ public class ClienteService {//todo DTOs add Authentication
         if (cliente == null)
             throw new GobsEntityNotFoundException(id, "Falha ao obter Cliente, Cliente não existe");
 
-        return Response.ok(cliente).build();//todo dto
+        return Response.ok(cliente.toDto()).build();
     }
 }
