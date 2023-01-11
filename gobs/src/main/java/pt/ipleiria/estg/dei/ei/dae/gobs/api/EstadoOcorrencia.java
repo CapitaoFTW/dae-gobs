45f4a92dbf8643b05857015144dc5bcb7b1a4d98
Rúben Garcia <rube200@users.noreply.github.com>
2023-01-11 21:33:13 +0000

    EstadoOcorrencia from value

--- gobs/src/main/java/pt/ipleiria/estg/dei/ei/dae/gobs/api/EstadoOcorrencia.java
+++ gobs/src/main/java/pt/ipleiria/estg/dei/ei/dae/gobs/api/EstadoOcorrencia.java
@@ -1,5 +1,8 @@
 package pt.ipleiria.estg.dei.ei.dae.gobs.api;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum EstadoOcorrencia {
     Criada(1),
     ParaAnalise(2),
@@ -9,10 +12,22 @@ public enum EstadoOcorrencia {
     Concluida(6),
     Invalida(7);
 
+    private static final Map<Integer, EstadoOcorrencia> estadosMap = new HashMap<>();
+
+    static {
+        for (EstadoOcorrencia estadoOcorrencia : EstadoOcorrencia.values()) {
+            estadosMap.put(estadoOcorrencia.value, estadoOcorrencia);
+        }
+    }
+
     private final Integer value;
 
     EstadoOcorrencia(Integer value) {
         this.value = value;
+    }
+
+    public static EstadoOcorrencia fromValue(Integer value) {
+        return estadosMap.get(value);
     }
 
     public int getValue() {
